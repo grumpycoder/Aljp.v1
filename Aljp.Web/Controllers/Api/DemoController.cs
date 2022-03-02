f7c25096cfd84bfdfd77f4d2fffa5e85470196bd
Mark Lawrence <marklawrence72@gmail.com>
2022-03-02 17:06:58 -0600

    added exception filters

---
+++ Aljp.Web/Controllers/Api/DemoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]", Name = "Demo")]
+public class DemoController : ControllerBase
+{
+    public async Task<IActionResult> Get()
+    {
+        return Ok(new
+        {
+            Name = "Demo",
+            Message = "Demo message text"
+        });
+    }
+}
---
+++ Aljp.Web/MvcDependencyInjection.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json.Serialization;
+using Aljp.Web.Filters;
+using DateOnlyTimeOnly.AspNet.Converters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace Aljp.Web;
+
+public static class MvcDependencyInjection
+{
+    public static IServiceCollection AddMvcDependencyInjection(this IServiceCollection services)
+    {
+        services
+            .AddControllersWithViews(options =>
+                options.Filters.Add<ApiExceptionFilterAttribute>()
+            )
+            .AddJsonOptions(
+                options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                }
+            ).ConfigureApiBehaviorOptions(options => { options.SuppressModelStateInvalidFilter = true; });
+
+        services.AddRazorPages();
+
+        services.AddApiVersioning(options =>
+        {
+            options.ReportApiVersions = true;
+            options.UseApiBehavior = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.DefaultApiVersion = new ApiVersion(1, 0);
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new QueryStringApiVersionReader(), //defaults to "api-version"
+                new QueryStringApiVersionReader("v"),
+                new HeaderApiVersionReader("api-version"),
+                new HeaderApiVersionReader("v"),
+                new MediaTypeApiVersionReader(), //defaults to "v"
+                new MediaTypeApiVersionReader("api-version"));
+        });
+
+        services.AddVersionedApiExplorer(
+            options =>
+            {
+                // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                // NOTE: the specified format code will format the version as "'v'major[.minor][-status]"
+                options.GroupNameFormat = "'v'VVV";
+                // NOTE: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                // can also be used to control the format of the API version in route templates
+                options.SubstituteApiVersionInUrl = true;
+            });
+
+        services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
+
+        return services;
+    }
+}
