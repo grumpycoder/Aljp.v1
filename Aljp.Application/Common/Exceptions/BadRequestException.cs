1aa54535ee3641c08384aef838d463f1fed17390
Mark Lawrence <marklawrence72@gmail.com>
2022-03-02 16:57:05 -0600

    added custom exceptions

---
+++ Aljp.Application/Common/Exceptions/BadRequestException.cs
@@ -0,0 +1,24 @@
+﻿namespace Aljp.Application.Common.Exceptions;
+
+public class BadRequestException: Exception
+{
+    public BadRequestException()
+        : base()
+    {
+    }
+
+    public BadRequestException(string message)
+        : base(message)
+    {
+    }
+
+    public BadRequestException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public BadRequestException(string name, object key)
+        : base($"Entity \"{name}\" ({key}) was not found.")
+    {
+    }
+}
