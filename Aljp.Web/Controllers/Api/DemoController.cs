8a248b1189f824fe7c9c0b8db81f21e2655bf622
Mark Lawrence <grumpycoder@outlook.com>
2022-03-03 08:33:58 -0600

    initial controller context connection

--- Aljp.Web/Controllers/Api/DemoController.cs
+++ Aljp.Web/Controllers/Api/DemoController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aljp.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [ApiController]
 [Route("api/[controller]", Name = "Demo")]
 public class DemoController : ControllerBase
 {
+    private readonly ApplicationDbContext _context;
+
+    public DemoController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
     public async Task<IActionResult> Get()
     {
+        var list = await _context.Vendors.ToListAsync();
+        return Ok(list);
+        
         return Ok(new
         {
             Name = "Demo",
